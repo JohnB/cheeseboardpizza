4b4d5dc91e60d8fab3939d271e0821b145e7bf6f
JohnB <john.baylor@gmail.com>
2016-12-10 14:49:44 -0800

    Update to handle new 2016/12/10 site redesign. Disabled 'spring' gem so I could load rails. Commented out salads because they aren't listed on the site.

--- Gemfile
+++ Gemfile
@@ -5,9 +5,8 @@ gem 'twitter', :git => 'https://github.com/sferik/twitter.git', :branch => 'stre
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
-# Use sqlite3 as the database for Active Record
-#gem 'sqlite3' 
-gem 'pg' 
+# Use postgres on Heroku
+gem 'pg'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
@@ -26,19 +25,3 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
-
-# Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-gem 'spring',        group: :development
-
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
-# Use debugger
-# gem 'debugger', group: [:development, :test]
-
--- app/models/pizza_of_the_day.rb
+++ app/models/pizza_of_the_day.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 #
 # NOTE: This code is very brittle and will break if the Cheese Board Collective
-# changes the format of its page in even a small way.
+# changes the format of its page (which has happend multiple times).
 #
 # Ideally, they would add a class to each pizza description that would tell us
 # what day the pizza is served (e.g. "<p class='tuesday'>Roasted potatoes..</p>")
@@ -13,8 +13,8 @@ class PizzaOfTheDay
   DAY_MATCH = /\d+\/\d+/
 
   def cheeseboard_date(time = Time.now)
-    # "Saturday 2/9", not "Saturday 02/09"
-    time.strftime("%A %-m/%-d")
+    # "Sat Dec 10" (re-assess in the first week of January 2017 - "Jan 1" or "Jan 01"?)
+    time.strftime("%a %h %-d")
   end
 
   def pizza_page
@@ -22,55 +22,43 @@ class PizzaOfTheDay
   end
 
   def pizza_days
-    # This scraping script is a lot simpler than I remember - only one selection via hpricot.
-    # The salient piece of HTML looks like this, and we only care about the innerHtml:
+    # The salient piece of HTML looks like this as of 12/10/2016 (but they change it sometimes):
     #
-    #   <div class="columns">
-    #	    <div class="column">
-    #       <h3><strong>The Week&rsquo;s Pizza</strong></h3>
-    #       <h4><label for="pizza_week_wednesday_pizza"></label></h4>		
-    #       <h4>Tuesday 10/28</h4>
-    #       <p>Zucchini, onion, mozzarella and other stuff..</p>
-    #       <h4>Wednesday 10/29</h4>
-    #       <p>Baby dino kale, baby swiss chard, and other stuff..</p>
+    #   <div class="pizza-list">
+    #     <article>
+    #       <div class="date">
+    #         <p>Sat Dec 10</p>
+    #       </div>
+    # 			<div class="menu">
+    # 				<h3>Pizza:</h3>
+    #         <p>Crushed tomato, red onion, ... and oregano</p>
+    # 			</div>
+    # 			<hr>
+    # 		</article>
+    #     <article>
     #       ...
     #
-    @pizza_days ||= (pizza_page/"div.columns"/"div.column").first
-  end
-
-  def lines_that_may_have_dates_or_toppings
-    @lines_that_may_have_dates_or_toppings = pizza_days.
-        children.
-        collect {|i| i.inner_text.gsub(/\s+/,' ').strip }.
-        reject {|i| i == "" }
+    @pizza_days ||= (pizza_page/".pizza-list"/ "article")
   end
 
   def potential_pizza_dates
     (-6..6).collect { |delta| delta.days.from_now }
   end
 
-  def topping_for_a_day(day)
-    day_to_look_for = cheeseboard_date(day)
-    lines_that_may_have_dates_or_toppings.each_with_index do |line, idx|
-      return lines_that_may_have_dates_or_toppings[idx+1] if line == day_to_look_for
-    end
-    nil
-  end
+  def days_and_pizzas
+    days = (pizza_days / "article" / ".date").children.collect(&:text)
+    pizzas = (pizza_days / ".menu" / "p").children.collect(&:text)
 
-  def salad_for_a_day(day)
-    day_to_look_for = cheeseboard_date(day)
-    lines_that_may_have_dates_or_toppings.each_with_index do |line, idx|
-      if line == day_to_look_for
-        salad = lines_that_may_have_dates_or_toppings[idx+2] || ""
-        return salad.sub(/^Salad\:\s*/,'')
-      end
-    end
-    nil
+    result = {}
+    days.each_with_index { |day, index| result[day] = pizzas[index] }
+    result
   end
 
   def days_with_pizza
     @days_with_pizza ||= potential_pizza_dates.inject({}) do |hash, day|
-      topping = topping_for_a_day(day)
+      day_to_look_for = cheeseboard_date(day)
+
+      topping = days_and_pizzas[day_to_look_for]
       if topping
         topping.gsub!(/ and /,' & ')
         topping.gsub!(/\*/,' ')   # as of 4/2015, asterisks are, apparently, not allowed!?
@@ -96,14 +84,40 @@ class PizzaOfTheDay
     pizza_of_the_day_with_time(today)[0..139]
   end
 
-  def salad_tweet_text(today = Time.now)
-    salad = salad_for_a_day(today)
-    if salad.blank?
-      return nil
-    end
-    salad.gsub!(/ and /,' & ')
-    salad.gsub!(/\*/,' ')   # as of 4/2015, asterisks are, apparently, not allowed!?
-    salad.squeeze!(' ')
-    today.strftime("%m/%d Salad: ") + salad
-  end
+  # def lines_that_may_have_dates_or_toppings
+  #   @lines_that_may_have_dates_or_toppings = pizza_days.
+  #       children.
+  #       collect {|i| i.inner_text.gsub(/\s+/,' ').strip }.
+  #       reject {|i| i == "" }
+  # end
+  #
+  # def topping_for_a_day(day)
+  #   day_to_look_for = cheeseboard_date(day)
+  #   lines_that_may_have_dates_or_toppings.each_with_index do |line, idx|
+  #     return lines_that_may_have_dates_or_toppings[idx+1] if line == day_to_look_for
+  #   end
+  #   nil
+  # end
+  #
+  # def salad_for_a_day(day)
+  #   day_to_look_for = cheeseboard_date(day)
+  #   lines_that_may_have_dates_or_toppings.each_with_index do |line, idx|
+  #     if line == day_to_look_for
+  #       salad = lines_that_may_have_dates_or_toppings[idx+2] || ""
+  #       return salad.sub(/^Salad\:\s*/,'')
+  #     end
+  #   end
+  #   nil
+  # end
+  #
+  # def salad_tweet_text(today = Time.now)
+  #   salad = salad_for_a_day(today)
+  #   if salad.blank?
+  #     return nil
+  #   end
+  #   salad.gsub!(/ and /,' & ')
+  #   salad.gsub!(/\*/,' ')   # as of 4/2015, asterisks are, apparently, not allowed!?
+  #   salad.squeeze!(' ')
+  #   today.strftime("%m/%d Salad: ") + salad
+  # end
 end
